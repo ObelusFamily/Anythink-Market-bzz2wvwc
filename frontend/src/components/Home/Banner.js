97477a4c8c4e247d2d17cba03cbcbd7bd65807b5
Michael Irwin <mikesir87@gmail.com>
2022-12-12 16:12:14 -0500

    Hide the input field

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = ({ onSearchUpdate }) => {
   const [searchParam, setSearchParam] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
 
   useEffect(() => {
     const param = searchParam.length >= 3 ? searchParam : "";
@@ -14,15 +15,21 @@ const Banner = ({ onSearchUpdate }) => {
     );
   }, [onSearchUpdate, searchParam]);
 
+  const onGetClick = useCallback((e) => {
+    e.preventDefault();
+    setShowSearch(true);
+  }, [setShowSearch]);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span>A place to </span>
-          <span id="get-part">get</span>
+          <span id="get-part" onClick={onGetClick}>get</span>
           &nbsp;
           <input
+            style={{display: showSearch ? "inline-block" : "none"}}
             type="text"
             onChange={(e) => setSearchParam(e.target.value)}
             value={searchParam}
